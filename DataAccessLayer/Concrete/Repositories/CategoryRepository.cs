53511cae9a5650f8bed61109a7f7602d4c731fde
Umut Özbilek <umutozbilek@hotmail.com>
2024-04-02 11:48:57 +0300

    Repository classes added

--- DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,11 @@ namespace DataAccessLayer.Concrete.Repositories
             c.SaveChanges();
         }
 
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Insert(Category p)
         {
             _object.Add(p);
@@ -31,6 +37,11 @@ namespace DataAccessLayer.Concrete.Repositories
         public List<Category> List()
         {
             return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
 
         public void Update(Category p)
