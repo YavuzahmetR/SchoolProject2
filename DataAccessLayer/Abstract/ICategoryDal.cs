bf3b759611f062591fa0822942c2cde994621d8d
Umut Özbilek <umutozbilek@hotmail.com>
2024-04-02 11:48:37 +0300

    IRepository added

--- DataAccessLayer/Abstract/ICategoryDal.cs
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal : IRepository<Category>
     {
-        List<Category> List();
+        //List<Category> List();
 
-        void Insert(Category p);
+        //void Insert(Category p);
 
-        void Update(Category p);
+        //void Update(Category p);
 
-        void Delete(Category p);
+        //void Delete(Category p);
     }
 }
